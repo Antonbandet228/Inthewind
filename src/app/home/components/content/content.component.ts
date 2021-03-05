da827d22fe9f04b6c45f153c5fd0bd2c38c62c42
Anton <toni.kovalev.98@list.ru>
2021-03-05 15:44:46 +0300

    home page

---
+++ src/app/home/components/content/content.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent implements OnInit {
+  posts = [{id: 0, text: 'text o'}, {id: 1, text: 'text 1'}, {id: 2, text: 'text 2'}, {id: 3, text: 'text 3'}];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClick(): void {
+    console.log('fjfggbhgkjg');
+  }
+
+}
+
